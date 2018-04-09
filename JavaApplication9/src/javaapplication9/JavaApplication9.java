a55021ac89427db2130a160cf3d89508800fdd66
stampstang5415 <stampstang5415@gmail.com>
2018-04-09 16:45:42 +0700

    Game

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -10,12 +10,13 @@ package javaapplication9;
  * @author STD
  */
 public class JavaApplication9 {
-
+ 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("5730213003");
     }
     
 }
