8355a3b220b49c51a13f66a552a4f750ecfd9ba0
Prakasit004 <Prakasit004@users.noreply.github.com>
2018-04-09 16:51:04 +0700

    commit

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -17,6 +17,7 @@ public class JavaApplication9 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("5730213003");
+        System.out.println("5730213004");
     }
     
 }
