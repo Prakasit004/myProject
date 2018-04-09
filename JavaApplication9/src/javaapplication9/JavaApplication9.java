1e1c451cd195b0c8a37732217f66e7f5365600b3
Araya_001 <s5730213001@phuket.psu.ac.th>
2018-04-09 17:18:57 +0700

    Merge origin/master
    Conflicts:
    	JavaApplication9/src/javaapplication9/JavaApplication9.java

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -23,6 +23,7 @@ public class JavaApplication9 {
         System.out.println("หมาตาย");
          System.out.println("หยกตาย");
           System.out.println("กายตาย");
+           System.out.println("กูเอง กูตายเอง");
          
     }
     
